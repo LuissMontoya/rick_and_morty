a360572c4b99b8580fbd3113deba578f452ed46f
Luiss Montoya <luisgoemon@gmail.com>
2021-09-02 22:57:13 -0500

    Proyecto Rick

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Rick And Morty</h1>
+    <FilterByStatus />
+    <FilterByName />
+    <ListCharacters />
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListCharacters from "@/components/ListCharacters";
+import FilterByStatus from "@/components/FilterByStatus";
+import FilterByName from "@/components/FilterByName";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ListCharacters,
+    FilterByName,
+    FilterByStatus,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+:root {
+  --background-body: #24282f;
+  --background-card: #3c3e44;
+  --text-white: #ffffff;
+  --text-gray: #c0c0c0;
+  --text-orange: #ff9800;
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: var(--background-body);
+  color: var(--text-white);
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+h1 {
+  margin-bottom: 3rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  width: 980px;
+  max-width: 90%;
+  margin: 5rem auto;
+}
+img {
+  width: 100%;
 }
 </style>
